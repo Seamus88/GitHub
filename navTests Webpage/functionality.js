9d1e1ac3cb2cd8c031dcc123ddc6efe5f95b88bb
James C <j.cosgrove88@icloud.com>
2018-04-15 12:53:04 -0500

    index functionality and styles updates

--- navTests Webpage/functionality.js
+++ navTests Webpage/functionality.js
@@ -1,14 +1,23 @@
-/*$(function() {
-	$('#submenu').hide();
-	$("#links").on("mouseover", function(){
-		$("#submenu").show();
-	})
-	$("#links").on("mouseleave", function() {
-		$("#submenu").hide();
-	})
+$(function() {
+	var name = "James Cosgrove";
+	var title = "Student";
+	var projects = [{
+		"name": "Pixel Art Maker",
+		"date": "January 2018",
+		"description": "Grow with Google Scholarship final project. A pixel art maker with JavaScript, jQuery, HTML, and CSS. 1 of 50000 scholarships out of those 50000 scholars awaiting to find out if part of the top 1000 to move on to the second part of the scholarship and get accepted to the FullStack Web Developer Course. "
+		"link": "https://codepen.io/Seamus88/pen/qpgmJg"
+	}, {
+		"name": "Random Quote Generator",
+		"date": "Winter 2017",
+		"description": "A random quote generator I made while doing a freeCodeCamp course.",
+		"link": "https://codepen.io/Seamus88/pen/yPBKJJ"
+	}];
+	
 
-	//$("#links").on("click", function() {
-	//	$("#submenu").toggle();
-	//})
+
+
+	$("main").append("<h1>" + name + "</h1>");
+	$("main").append("<h2>" + title + "</h2>");
+
+	
 })
-*/
