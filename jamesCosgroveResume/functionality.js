9152d2f99ab9d541d4d92d9681b2e57516ff427d
James C <j.cosgrove88@icloud.com>
2018-04-17 12:09:50 -0500

    minor updates

--- jamesCosgroveResume/functionality.js
+++ jamesCosgroveResume/functionality.js
@@ -12,6 +12,12 @@ $(function() {
 		"description": "A random quote generator I made while doing a freeCodeCamp course.",
 		"link": "https://codepen.io/Seamus88/pen/yPBKJJ"
 	}];
-
+	var schooling = [{
+		school: "Joliet Central High School",
+		graduated: 2006
+	}, {
+		school: "Truman College",
+		graduated: "Currently Attending"
+	}]
 	
 })
