68baeaa82233975016910ded7a7d5bcc30ec711c
Seamus88 <J.Cosgrove88@icloud.com>
2018-04-17 19:04:22 -0600

    functionality quote removal and index div updates

--- jamesCosgroveResume/functionality.js
+++ jamesCosgroveResume/functionality.js
@@ -2,15 +2,15 @@ $(function() {
 	var name = "James Cosgrove";
 	var title = "Student";
 	var projects = [{
-		"name": "Pixel Art Maker",
-		"date": "January 2018",
-		"description": "Grow with Google Scholarship final project. A pixel art maker with JavaScript, jQuery, HTML, and CSS. 1 of 50000 scholarships out of those 50000 scholars awaiting to find out if part of the top 1000 to move on to the second part of the scholarship and get accepted to the FullStack Web Developer Course. "
-		"link": "https://codepen.io/Seamus88/pen/qpgmJg"
+		name: "Pixel Art Maker",
+		date: "January 2018",
+		description: "Grow with Google Scholarship final project. A pixel art maker with JavaScript, jQuery, HTML, and CSS. 1 of 50000 scholarships out of those 50000 scholars awaiting to find out if part of the top 1000 to move on to the second part of the scholarship and get accepted to the FullStack Web Developer Course. "
+		link: "https://codepen.io/Seamus88/pen/qpgmJg"
 	}, {
-		"name": "Random Quote Generator",
-		"date": "Winter 2017",
-		"description": "A random quote generator I made while doing a freeCodeCamp course.",
-		"link": "https://codepen.io/Seamus88/pen/yPBKJJ"
+		name: "Random Quote Generator",
+		date: "Winter 2017",
+		description: "A random quote generator I made while doing a freeCodeCamp course.",
+		link: "https://codepen.io/Seamus88/pen/yPBKJJ"
 	}];
 	var schooling = [{
 		school: "Joliet Central High School",
